692583e527c12d6729ce4788a340a314b82912de
polyblendsolutions <polyblend.solution@gmail.com>
2024-04-22 23:54:51 +0530

    comments resolved

--- src/shared/utils/utils.service.ts
+++ src/shared/utils/utils.service.ts
@@ -158,12 +158,7 @@ export class UtilsService {
   }
 
   generateUniqueId(inputId: string): string {
-    // Extract the first two characters and last four characters
-    var prefix = inputId.substring(0, 2);
-    var suffix = inputId.substring(inputId.length - 4);
-
-    // Combine the prefix and suffix to form the new unique ID
-    var newId = prefix + suffix;
-    return newId;
+    // Combine the prefix(2 initials from the start) and suffix(4 characters from the last) to form the new unique ID
+    return inputId.substring(0, 2) + inputId.substring(inputId.length - 4);
   }
 }
