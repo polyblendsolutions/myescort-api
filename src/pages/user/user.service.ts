83f9838edb6cc5f62e7763c2a86595f7ec62d82a
polyblendsolutions <polyblend.solution@gmail.com>
2024-05-02 23:10:13 +0530

    new registration template (#33)
    * new registration template
    
    * Fix
    
    ---------
    
    Co-authored-by: Seweryn Szymon <54148927+trol98@users.noreply.github.com>

--- src/pages/user/user.service.ts
+++ src/pages/user/user.service.ts
@@ -240,15 +240,79 @@ export class UserService {
         // Cache Removed
         await this.cacheManager.del(this.cacheAllData);
         await this.cacheManager.del(this.cacheDataCount);
-
         // email send
-        // this.emailService.sendEmail(
-        //   createUserDto['email'],
-        //   `
-        // <h5>We are checking your information.</h5>
-        // <p>Thank You</p>
-        // `,
-        // );
+        this.emailService.sendEmail(
+          createUserDto.email,
+          `<body style="margin: 0px;background-color: #f658a8;">
+          <div
+              style="background-color: #fff;background-repeat: no-repeat;background-size: cover;min-height:100vh">
+              <table style="width:100%;margin:0 auto;max-width:660px;">
+                  <tr>
+                      <td style="height:50px" colspan="2"></td>
+                  </tr>
+                  <tr>
+                
+                  <td colspan="2">
+      
+                      <table
+                          style="width:100%;margin: auto;background-color: #fff;padding: 30px;border-radius: 15px; border: 2px solid #f658a8;">
+      
+                              <tr style="text-align: center; background-color: #f658a8;">
+                                  <td
+                                      style="margin: 0px;padding: 40px;font-family:Arial, Helvetica, sans-serif;color:#fff;font-size: 21px;text-align: center;border-radius: 15px;">
+                                      
+                                  <h3
+                                  style="margin: 0px;font-family:Arial, Helvetica, sans-serif;color:#fff;font-size: 21px;text-align: center;">
+                                  We are checking your information.</h3>
+                                  </td>
+                              </tr>
+                          
+                          <tr style="height: 20px;">
+                              <td></td>
+                          </tr>
+                          <tr>
+                              <td>
+                                  <p
+                                      style="margin: 0px;font-family:Arial, Helvetica, sans-serif;line-height: 28px;color: #646464;text-align: center;">
+                                      Thank You
+                                  </p>
+                              </td>
+                          </tr>
+                          <tr style="height: 40px;">
+                              <td></td>
+                          </tr>
+                          <tr style="  height: 20px;">
+                              <td></td>
+                          </tr>
+                          <tr style="background: #f658a8;  height: 1px;">
+                              <td></td>
+                          </tr>
+                          <tr style="  height: 20px;">
+                              <td></td>
+                          </tr>
+                          <tr>
+                              <td>
+                                  <p
+                                      style="margin: 0px;font-family:Arial, Helvetica, sans-serif;font-size: 12px;color: #888888;text-align: center;">
+                                      © Copyright 2022 - 2023 MyEscort. All Rights Reserved</p>
+                              </td>
+                          </tr>
+                          <tr>
+                              <td colspan="1"></td>
+                          </tr>
+                      </table>
+                  </td>
+                  </tr>
+      
+                  <tr>
+                      <td style="height:50px" colspan="2"></td>
+                  </tr>
+              </table>
+      
+          </div>
+      </body>
+        `,'Registration'
+        );
 
         return this.userLogin(authUserDto);
       }
